62448219384382f7a6ed7dcdadcec65683983794
lovzay <tfvin24@gmail.com>
2024-01-05 12:35:54 +0800

    define next turn

--- main.py
+++ main.py
@@ -33,7 +33,45 @@ class Food:
         canvas.create_oval(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=FOOD_COLOR, tag="food")
 
     def next_turn(snake, food):
-        pass
+        x, y = snake.coordinates[0]
+
+        if direction == "up":
+            y -= SPACE_SIZE
+        elif direction == "down":
+            y += SPACE_SIZE
+        elif direction == "left":
+            x -= SPACE_SIZE
+        elif direction == "right":
+            x += SPACE_SIZE
+
+        snake.coordinates.insert(0, (x, y))
+
+        square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR)
+
+        snake.squares.insert(0, square)
+
+        if x == food.coordinates[0] and y == food.coordinates[1]:
+
+            global score
+            score += 1
+            label.config(text="Score:{}".format(score))
+
+            canvas.delete("food")
+
+            food = Food()
+
+        else:
+            del snake.coordinates[-1]
+
+            canvas.delete(snake.squares[-1])
+
+            del snake.squares[-1]
+
+        if check_collision(snake):
+            game_over()
+
+        else:
+            window.after(SPEED, next_turn, snake, food)
 
     def change_direction(new_direction):
         pass
