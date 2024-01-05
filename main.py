4e649dd27c48b7d3bfd3d7f70ffc0a2e8d082ba2
lovzay <tfvin24@gmail.com>
2024-01-05 12:29:38 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+from tkinter import *
+import random
+
+GAME_WIDTH: int = 900
+GAME_HEIGHT: int = 900
+SPEED = 80
+SPACE_SIZE: int = 60
+BODY_PARTS = 3
+SNAKE_COLOR = "#00FF00"
+FOOD_COLOR = "#FF0000"
+BACKGROUND_COLOR = "#000000"
+
+
+    class Snake:
+
+    class Food:
+
+
+    def next_turn(snake, food):
+        pass
+
+    def change_direction(new_direction):
+        pass
+
+    def check_collision(snake):
+        pass
+    def game_over():
+        pass
+
