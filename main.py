63be59aa0f663b18a9bc6310babb032ac49a284f
lovzay <tfvin24@gmail.com>
2024-01-05 12:36:30 +0800

    define check collision

--- main.py
+++ main.py
@@ -93,7 +93,18 @@ class Food:
                 direction = new_direction
 
     def check_collision(snake):
-        pass
+        x, y = snake.coordinates[0]
+
+        if x < 0 or x >= GAME_WIDTH:
+            return True
+        elif y < 0 or y >= GAME_HEIGHT:
+            return True
+
+        for body_part in snake.coordinates[1:]:
+            if x == body_part[0] and y == body_part[1]:
+                return True
+
+        return False
     def game_over():
         pass
 
